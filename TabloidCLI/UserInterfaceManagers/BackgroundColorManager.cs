dc740fbd57f7682bdf37766cb25b50c6b2a62681
Iris Lee <irislee4git@gmail.com>
2024-08-14 13:40:04 -0400

    add background color menu

---
+++ TabloidCLI/UserInterfaceManagers/BackgroundColorManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    internal class BackgroundColorManager : IUserInterfaceManager
+    {
+        private IUserInterfaceManager _parentUI;
+
+        public BackgroundColorManager(IUserInterfaceManager parentUI, string connectionString)
+        {
+            _parentUI = parentUI;
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("Background Color Selection");
+            Console.WriteLine(" 1) Dark Cyan");
+            Console.WriteLine(" 2) Dark Yellow");
+            Console.WriteLine(" 0) Go Back");
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    Console.BackgroundColor =       ConsoleColor.DarkCyan;
+                    Console.Clear();
+                    return this;
+                case "2":
+                    Console.BackgroundColor = ConsoleColor.DarkYellow;
+                    Console.Clear();
+                    return this;
+                case "0":
+                    return _parentUI;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    return this;
+
+            }
+        }
+    }
+}
